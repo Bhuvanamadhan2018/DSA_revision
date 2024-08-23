41ad703060003fdfd70e29306960b4cf2a1238d1
Bhuvanamadhan2018 <bhuvanamadhan19@gmail.com>
2024-08-23 11:48:57 +0530

    array problems

---
+++ Arrays/subArraysum.py
@@ -0,0 +1,22 @@
+def SubArraySumEqualsTarget(arr,target):
+    left= 0
+    # right = 0
+    current_sum = 0
+    
+    for right in range(len(arr)):
+        current_sum+=arr[right]
+
+        while current_sum > target and left <= right:
+            # decreasing  left pointer value from current_sum
+            current_sum -= arr[left]
+            # increment left pointer to right side of array
+            left +=1  
+
+        if current_sum == target:
+            return f"Target equals to sum of numbers is between indices {left} and {right}"
+    return None
+
+arr = [3,4,5,6,2,9,6]
+target = 17
+result = SubArraySumEqualsTarget(arr,target)
+print(result)
