e8516b71319d7efa0f404e25db1e230763dfdbb3
Bhuvanamadhan2018 <bhuvanamadhan19@gmail.com>
2024-08-27 11:55:31 +0530

    modified

--- Binarysearch_algorithm/searchMatrix.py
+++ Binarysearch_algorithm/searchMatrix.py
@@ -1,26 +1,25 @@
 from typing import List
 
-def search2DMatrix(matrix,target):
+def search2DMatrix(matrix: List[List[int]], target: int) -> bool:
     n = len(matrix[0])
     m = len(matrix)
-
     left = 0
-    right = (m*n)-1
+    right = (m * n) - 1
 
     while left <= right:
-        mid = (left + right)//2
-        mid_val = matrix[mid//n][mid%n]
+        mid = (left + right) // 2
+        mid_val = matrix[mid // n][mid % n]
+
         if target == mid_val:
-            return 
+            return True
         elif target < mid_val:
-            left = mid + 1
-        elif target > mid_val:
             right = mid - 1
         else:
-            return True
+            left = mid + 1
+
     return False
 
-matrix =[[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-target = 3
-result = search2DMatrix(matrix,target)
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+target = 15
+result = search2DMatrix(matrix, target)
 print(result)
